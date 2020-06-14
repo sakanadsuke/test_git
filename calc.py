103145e2144f684d11aa6026619bc146bcc605b5
daisuke saito <dsaito@saitoudaisukenoMacBook-Pro.local>
2020-06-14 23:51:00 +0900

    add subtract func

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
 def sum(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 val_1 = input('input first number: ')
 val_2 = input('input second number: ')
 
@@ -9,3 +12,9 @@ print("")
 
 print("the result of sum =")
 print(value_sum)
+
+value_subtract = subtract(int(val_1), int(val_2))
+
+print("the result of subtract =")
+print(value_subtract)
+
